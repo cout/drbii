54795d9cb39a697ea3dc65b8c678822cdbf5de9b
Paul Brannan <curlypaul924@gmail.com>
2010-12-27 22:36:28 -0500

    Added test script

---
+++ bin/test1.rb
@@ -0,0 +1,16 @@
+require 'smec/smec'
+require 'smec/memory_map'
+require 'smec/ftdi_io'
+
+if __FILE__ == $0 then
+  source_filename = ARGV[0]
+  mmap = MemoryMap.new(source_filename)
+
+  ftdi_dev = ARGV[1]
+  ftdi = FtdiIO.new(ftdi_dev, true)
+
+  smec = SMEC.new(ftdi)
+  smec.handshake()
+  puts mmap.read_location(mmap['RPM'])
+end
+
