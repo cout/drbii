a1fbb785a46997b153f8e55594be21a8d7bed82a
Paul Brannan <curlypaul924@gmail.com>
2010-12-27 22:34:46 -0500

    More complete ftdi extension

--- ext/ftdi/ftdi.c
+++ ext/ftdi/ftdi.c
@@ -16,12 +16,15 @@ static void check_ftdi_result(struct ftdi_context * ctx, int result)
 static VALUE ftdi_context_s_new(VALUE klass)
 {
   /* TODO: not exception-safe */
-  /* TODO: check for null return value */
-  /* TODO: need to call init? */
-  struct ftdi_context * ctx = ftdi_new();
-  VALUE v_ctx = Data_Wrap_Struct(rb_cFtdi_Context, 0, ftdi_free, ctx);
 
-  return v_ctx;
+  struct ftdi_context * ctx = ftdi_new();
+
+  if (ctx == 0)
+  {
+    rb_raise(rb_eRuntimeError, "unable to create ftdi context");
+  }
+ 
+  return Data_Wrap_Struct(rb_cFtdi_Context, 0, ftdi_free, ctx);
 }
 
 static VALUE ftdi_context_open(VALUE self, VALUE v_desc)
@@ -37,6 +40,43 @@ static VALUE ftdi_context_open(VALUE self, VALUE v_desc)
   check_ftdi_result(ctx, result);
 
   return Qnil;
+}
+
+static VALUE ftdi_context_close(VALUE self)
+{
+  struct ftdi_context * ctx;
+  int result;
+
+  Data_Get_Struct(self, struct ftdi_context, ctx);
+
+  result = ftdi_usb_close(ctx);
+  check_ftdi_result(ctx, result);
+
+  return Qnil;
+}
+
+static VALUE ftdi_context_type(VALUE self)
+{
+  struct ftdi_context * ctx;
+
+  Data_Get_Struct(self, struct ftdi_context, ctx);
+
+  return INT2NUM(ctx->type);
+
+}
+
+static VALUE ftdi_context_chipid(VALUE self)
+{
+  struct ftdi_context * ctx;
+  int result;
+  unsigned int chipid;
+
+  Data_Get_Struct(self, struct ftdi_context, ctx);
+  result = ftdi_read_chipid(ctx, &chipid);
+  check_ftdi_result(ctx, result);
+
+  return UINT2NUM(chipid);
+
 }
 
 static VALUE ftdi_context_set_baudrate(VALUE self, VALUE v_baud)
@@ -108,6 +148,9 @@ void Init_ftdi()
   rb_cFtdi_Context = rb_define_class_under(rb_mFtdi, "Context", rb_cObject);
   rb_define_singleton_method(rb_cFtdi_Context, "new", ftdi_context_s_new, 0);
   rb_define_method(rb_cFtdi_Context, "open", ftdi_context_open, 1);
+  rb_define_method(rb_cFtdi_Context, "close", ftdi_context_close, 0);
+  rb_define_method(rb_cFtdi_Context, "type", ftdi_context_type, 0);
+  rb_define_method(rb_cFtdi_Context, "chipid", ftdi_context_chipid, 0);
   rb_define_method(rb_cFtdi_Context, "baudrate=", ftdi_context_set_baudrate, 1);
   rb_define_method(rb_cFtdi_Context, "set_bitmode", ftdi_context_set_bitmode, 2);
   rb_define_method(rb_cFtdi_Context, "read_data", ftdi_context_read_data, 1);
@@ -127,5 +170,42 @@ void Init_ftdi()
   rb_define_const(rb_mFtdi, "INTERFACE_B", INT2NUM(INTERFACE_B));
   rb_define_const(rb_mFtdi, "INTERFACE_C", INT2NUM(INTERFACE_C));
   rb_define_const(rb_mFtdi, "INTERFACE_D", INT2NUM(INTERFACE_D));
+
+  rb_define_const(rb_mFtdi, "TYPE_AM", INT2NUM(TYPE_AM));
+  rb_define_const(rb_mFtdi, "TYPE_BM", INT2NUM(TYPE_BM));
+  rb_define_const(rb_mFtdi, "TYPE_R", INT2NUM(TYPE_R));
+  rb_define_const(rb_mFtdi, "TYPE_2232H", INT2NUM(TYPE_2232H));
+  rb_define_const(rb_mFtdi, "TYPE_4232H", INT2NUM(TYPE_4232H));
+
+  rb_define_const(rb_mFtdi, "NONE", INT2NUM(NONE));
+  rb_define_const(rb_mFtdi, "ODD", INT2NUM(ODD));
+  rb_define_const(rb_mFtdi, "EVEN", INT2NUM(EVEN));
+  rb_define_const(rb_mFtdi, "MARK", INT2NUM(MARK));
+  rb_define_const(rb_mFtdi, "SPACE", INT2NUM(SPACE));
+
+  rb_define_const(rb_mFtdi, "STOP_BIT_1", INT2NUM(STOP_BIT_1));
+  rb_define_const(rb_mFtdi, "STOP_BIT_15", INT2NUM(STOP_BIT_15));
+  rb_define_const(rb_mFtdi, "STOP_BIT_2", INT2NUM(STOP_BIT_2));
+
+  rb_define_const(rb_mFtdi, "BITS_7", INT2NUM(BITS_7));
+  rb_define_const(rb_mFtdi, "BITS_8", INT2NUM(BITS_8));
+
+  rb_define_const(rb_mFtdi, "BREAK_OFF", INT2NUM(BREAK_OFF));
+  rb_define_const(rb_mFtdi, "BREAK_ON", INT2NUM(BREAK_ON));
+
+  rb_define_const(rb_mFtdi, "MPSSE_WRITE_NEG", INT2NUM(MPSSE_WRITE_NEG));
+  rb_define_const(rb_mFtdi, "MPSSE_BITMODE", INT2NUM(MPSSE_BITMODE));
+  rb_define_const(rb_mFtdi, "MPSSE_READ_NEG", INT2NUM(MPSSE_READ_NEG));
+  rb_define_const(rb_mFtdi, "MPSSE_LSB", INT2NUM(MPSSE_LSB));
+  rb_define_const(rb_mFtdi, "MPSSE_DO_WRITE", INT2NUM(MPSSE_DO_WRITE));
+  rb_define_const(rb_mFtdi, "MPSSE_DO_READ", INT2NUM(MPSSE_DO_READ));
+  rb_define_const(rb_mFtdi, "MPSSE_WRITE_TMS", INT2NUM(MPSSE_WRITE_TMS));
+
+  rb_define_const(rb_mFtdi, "SET_BITS_LOW", INT2NUM(SET_BITS_LOW));
+  rb_define_const(rb_mFtdi, "SET_BITS_HIGH", INT2NUM(SET_BITS_HIGH));
+  rb_define_const(rb_mFtdi, "GET_BITS_LOW", INT2NUM(GET_BITS_LOW));
+  rb_define_const(rb_mFtdi, "GET_BITS_HIGH", INT2NUM(GET_BITS_HIGH));
+  rb_define_const(rb_mFtdi, "LOOPBACK_START", INT2NUM(LOOPBACK_START));
+  rb_define_const(rb_mFtdi, "TCK_DIVISOR", INT2NUM(TCK_DIVISOR));
 }
 
