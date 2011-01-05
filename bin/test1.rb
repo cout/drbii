4e5cb1bb914aedb7594dbd5c7b7c1df01126632b
Paul Brannan <curlypaul924@gmail.com>
2011-01-04 21:41:01 -0500

    Fixed bugs due to API changes. Added default for location of ftdi.

--- bin/test1.rb
+++ bin/test1.rb
@@ -1,16 +1,16 @@
-require 'drbii/smec'
+require 'drbii/drbii'
 require 'drbii/memory_map'
 require 'drbii/io/ftdi_io'
 
 if __FILE__ == $0 then
-  source_filename = ARGV[0]
-  mmap = MemoryMap.new(source_filename)
+  source_filename = ARGV[0] or raise "need source filename"
+  mmap = MemoryMap.read_source(source_filename)
 
-  ftdi_dev = ARGV[1]
+  ftdi_dev = ARGV[1] || 'i:0x0403:0x6001'
   ftdi = FtdiIO.new(ftdi_dev, true)
 
   drbii = DRBII.new(ftdi)
   drbii.handshake()
-  puts mmap.read_location(mmap['RPM'])
+  puts mmap['RPM'].read(drbii)
 end
 
